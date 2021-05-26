7e84b8032094c9fa2ad0277da1a946ac8f4dfaa1
pratyu10 <mpratyusha0@gmail.com>
2021-05-26 10:40:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/studentexcercisestageone/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.studentexcercisestageone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.light_background_activity)
+
+        // Change your layout file.
+
+    }
+}
